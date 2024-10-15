9b58eff1e319fb301f65e5b1e120aec5ceb83c75
Max-Kucher-Sar <kucherenko.max75@mail.ru>
2024-10-15 19:48:20 +0400

    module_9_4.py

--- module_9_4.py
+++ module_9_4.py
@@ -1,3 +1,5 @@
+from random import choice
+
 # Lambda-функция:
 first = 'Мама мыла раму'
 second = 'Рамена мало было'
@@ -7,12 +9,29 @@ print(result)
 # Замыкание:
 
 def get_advanced_writer(file_name):
-    with open(file_name, 'w') as file:
-        def write_everything(*data_set):
-            arguments = map(str, data_set)
-            file.write(arguments)
+    file = open(file_name, 'w', encoding='utf-8')
+    def write_everything(*data_set):
+        for data in data_set:
+            if isinstance(data, (list, tuple)):
+                file.write(str(data) + '\n')
+            else:
+                file.write(str(data) + '\n')
 
     return write_everything
 
 write = get_advanced_writer('example.txt')
-result_2 = write('Это строчка', ['А', 'это', 'уже', 'число', 5, 'в', 'списке'])
+write('Это строчка', ['А', 'это', 'уже', 'число', 5, 'в', 'списке'])
+
+# Метод __call__:
+
+class MysticBall:
+    def __init__(self, *word):
+        self.word = word
+
+    def __call__(self):
+        return choice(self.word)
+
+first_ball = MysticBall('Да', 'Нет', 'Наверное')
+print(first_ball())
+print(first_ball())
+print(first_ball())
